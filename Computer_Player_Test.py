a266c7128d35769bd8b5f4fb67e71836b1f1c2bf
Glenn Hurd <glennqhurd@gmail.com>
2016-07-07 14:07:42 -0400

    Started adding method that converts a board into a corresponding variable.

--- Computer_Player_Test.py
+++ Computer_Player_Test.py
@@ -11,5 +11,20 @@ class Computer_Player_test(unittest.TestCase):
             test_board.append([' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '])
         self.assertEqual(test_board, comp.get_board_info())
 
+    def test_get_variable(self):
+        comp = Computer_Player()
+        test_board = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
+        self.assertEqual('c', comp.get_variable(test_board))
+        test_board = ['X', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
+        self.assertEqual('1', comp.get_variable(test_board))
+        test_board = [' ', 'X', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
+        self.assertEqual('1', comp.get_variable(test_board))
+        test_board = [' ', ' ', ' ', ' ', 'X', ' ', ' ', ' ', ' ']
+        self.assertEqual('c2', comp.get_variable(test_board))
+        test_board = ['X', 'X', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
+        self.assertEqual('ad', comp.get_variable(test_board))
+        test_board = ['X', ' ', 'X', ' ', ' ', ' ', ' ', ' ', ' ']
+        self.assertEqual('b', comp.get_variable(test_board))
+
 if __name__ == '__main__':
     unittest.main()
